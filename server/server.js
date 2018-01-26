594730b42c4620bee9845c93acd8a78aba9925e8
bobby <bwuphan@gmail.com>
2018-01-25 19:25:33 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 'use strict';
 
+// const express = require('express');
+
+// // Constants
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
+
+// // App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello world\n');
+// });
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+const port = process.env.PORT || 3000;
+
+const rootPath = path.join(__dirname, '/..');
+
+app.use(express.static(rootPath));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log('Example app listening on port', port);
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+module.exports = app;
