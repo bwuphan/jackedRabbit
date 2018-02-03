29d431f4d5b4f8faa898f050acd1045af19016a7
bobby <bwuphan@gmail.com>
2018-02-03 14:01:58 -0800

    Import bootstrap in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Root from './Root';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // Auth0 API Keys -- file part of .gitignore
 // import AUTH0 from './config/auth0';
