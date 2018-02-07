84f7aaacbea8de8194793df45d6a27b3cba34a50
bobby <bwuphan@gmail.com>
2018-02-06 23:21:37 -0800

    WIP auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,3 @@
-import history from '../history';
 import auth0 from 'auth0-js';
 import { AUTH_CONFIG } from './auth0-variables';
 
@@ -27,9 +26,9 @@ export default class Auth {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        history.replace('/home');
+        console.log('logged in');
       } else if (err) {
-        history.replace('/home');
+
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
@@ -42,8 +41,8 @@ export default class Auth {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+    console.log('SESSION SET');
     // navigate to the home route
-    history.replace('/home');
   }
 
   logout() {
@@ -51,14 +50,15 @@ export default class Auth {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    console.log('LOGGED OUT');
     // navigate to the home route
-    history.replace('/home');
   }
 
   isAuthenticated() {
     // Check whether the current time is past the
     // access token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    console.log(expiresAt)
     return new Date().getTime() < expiresAt;
   }
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
-
+import { Navbar, Button } from 'react-bootstrap';
+import Auth from '../Auth/Auth';
 
 export default class Landing extends Component {
   constructor(props) {
     super(props);
+    this.auth = new Auth();
+
+    // const handleAuthentication = ({location}) => {
+    // if (/access_token|id_token|error/.test(location.hash)) {
+    //   auth.handleAuthentication();
+    // }
+  }
+
+  login() {
+    this.auth.login();
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
   render() {
+    const { isAuthenticated } = this.auth;
+    console.log(isAuthenticated())
     return (
       <div>
-        <h2>LANDING</h2>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    id="qsLogoutBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          </Navbar.Header>
+        </Navbar>
       </div>
     );
   }
