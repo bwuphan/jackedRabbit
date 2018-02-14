3bb6264d5cb973afdb45182a6e4b74ce3b6c1c9e
bobby <bwuphan@gmail.com>
2018-02-13 22:24:55 -0800

    Add Callback.js

---
+++ src/components/Callback/Callback.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Callback extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  return() {
+    <div>
+      <h2>Loading</h2>
+    </div>
+  }
+}
