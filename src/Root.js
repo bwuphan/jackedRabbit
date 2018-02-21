23851434570f262c0733b946ea25c84a0393efb1
bobby <bwuphan@gmail.com>
2018-02-20 21:24:49 -0800

    Add handleAuthentication to src/Root.js

--- src/Root.js
+++ src/Root.js
@@ -6,16 +6,25 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import App from './App';
 import Landing from './components/Landing/Landing';
 import Callback from './components/Callback/Callback';
+import Auth from './components/Auth/Auth';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
+
+const auth = new Auth();
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
 
 const Root = () =>
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-        <IndexRoute component={Landing} />
-        <Route path="/Callback" component={Callback} />
+        <IndexRoute component={Landing} auth={Auth}/>
+        <Route path="/Callback" component={Callback} auth={Auth}/>
       </Route>
     </Router>
   </Provider>;
