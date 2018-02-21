8b4005f29f85de201095c1f8add4e3fff3db6ffd
bobby <bwuphan@gmail.com>
2018-02-20 21:24:07 -0800

    Add handleAuthentication to src/components/Callback/Callback.js

--- src/components/Callback/Callback.js
+++ src/components/Callback/Callback.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Auth from '../Auth/Auth';
 
 export default class Callback extends Component {
   constructor(props) {
     super(props);
+    this.auth = new Auth();
   }
 
   render() {
+    const { handleAuthentication } = this.auth;
+    handleAuthentication(this.props);
     return (
       <div>
         <h2>Loading</h2>
